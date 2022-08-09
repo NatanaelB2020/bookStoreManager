fecfd958a8b95da583696262b2bcfe2ff900a7b1
Natanael Amaral de Barros <natan.amaral.barros@gmail.com>
2022-08-09 11:45:56 -0300

    criação das entidades

---
+++ bookStoreManager/src/main/java/com/example/bookStoreManager/entity/Author.java
@@ -0,0 +1,28 @@
+package com.example.bookStoreManager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false)
+    private Integer age;
+
+}
---
+++ bookStoreManager/src/main/java/com/example/bookStoreManager/entity/Book.java
@@ -0,0 +1,40 @@
+package com.example.bookStoreManager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer pages;
+
+    @Column(nullable = false)
+    private Integer chapters;
+
+    @Column(nullable = false)
+    private  String isbn;
+
+    @Column(name = "publisherName", nullable = false, unique = true)
+    private String publisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+}
